a646987583f76a5f0bc9307f84b42631e736cb35
Martin Piatka <piatka@cesnet.cz>
2026-02-24 11:24:07 +0100

    afilter/channel_remap.cpp: Remove config includes

--- src/audio/filter/channel_remap.cpp
+++ src/audio/filter/channel_remap.cpp
@@ -35,12 +35,7 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#include "config_unix.h"
-#include "config_win32.h"
-#endif /* HAVE_CONFIG_H */
-
+#include <cassert>
 #include <memory>
 #include <string_view>
 

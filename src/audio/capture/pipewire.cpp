f36f54f800a129adad9bed4dd95d63db42b6fae5
Martin Piatka <piatka@cesnet.cz>
2026-02-24 11:21:27 +0100

    acap/pipewire.cpp: Remove config includes

--- src/audio/capture/pipewire.cpp
+++ src/audio/capture/pipewire.cpp
@@ -35,15 +35,10 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#include "config_unix.h"
-#endif
-
+#include <cassert>
 #include <memory>
 #include <vector>
 #include <thread>
-#include <chrono>
 
 #include "audio/audio_capture.h"
 #include "audio/types.h"

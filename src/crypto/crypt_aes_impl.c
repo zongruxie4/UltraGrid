901452fb41bc890feeccd0e52cf9c4d682d6f797
Martin Piatka <piatka@cesnet.cz>
2026-02-24 13:02:32 +0100

    crypto/crypt_aes_impl: Remove config includes

--- src/crypto/crypt_aes_impl.c
+++ src/crypto/crypt_aes_impl.c
@@ -24,9 +24,6 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
-#include "config_unix.h"
-#include "config_win32.h"
 #include "debug.h"
 #include "crypt_aes_impl.h"
 #include "crypt_aes.h"

be5491ab26e11d247b4f80dec0dc71a95265b529
Martin Piatka <piatka@cesnet.cz>
2026-02-24 12:59:34 +0100

    crypto/crc.h: Remove prototypes for nonexistent functions

--- src/crypto/crc.h
+++ src/crypto/crc.h
@@ -19,26 +19,6 @@ extern "C" {
 #endif
 
 /*
-**  File: ARCCRC16.C
-*/
-
-void init_crc_table(void);
-uint16_t crc_calc(uint16_t crc, char *buf, unsigned nbytes);
-void do_file(char *fn);
-
-/*
-**  File: CRC-16.C
-*/
-
-uint16_t crc16(char *data_p, uint16_t length);
-
-/*
-**  File: CRC-16F.C
-*/
-
-uint16_t updcrc(uint16_t icrc, uint8_t *icp, size_t icnt);
-
-/*
 **  File: CRC_32.C
 */
 
@@ -51,19 +31,6 @@ bool crc32file(char *name, uint32_t *crc, long *charcnt);
 uint32_t crc32buf(const char *buf, size_t len);
 
 uint32_t crc32buf_with_oldcrc(const char *buf, size_t len, uint32_t oldcrc);
-
-/*
-**  File: CHECKSUM.C
-*/
-
-unsigned checksum(void *buffer, size_t len, unsigned int seed);
-
-/*
-**  File: CHECKEXE.C
-*/
-
-void checkexe(char *fname);
-
 
 #ifdef __cplusplus
 }

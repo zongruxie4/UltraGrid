53f680c6f4d30beecd2a08a15a7ab14e90de7236
Martin Pulec <martin.pulec@cesnet.cz>
2026-03-25 12:50:15 +0100

    vdisp/dummy: modernize + mv usage to separate fn

--- src/video_display/dummy.c
+++ src/video_display/dummy.c
@@ -3,7 +3,7 @@
  * @author Martin Pulec     <pulec@cesnet.cz>
  */
 /*
- * Copyright (c) 2015-2023 CESNET, z. s. p. o.
+ * Copyright (c) 2015-2026 CESNET, zájmové sdružení právnických osob
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -36,12 +36,12 @@
  */
 
 #include <assert.h>
-#include <stdbool.h>
 #include <stdio.h>            // for printf
 #include <stdlib.h>
 #include <string.h>
 
 #include "debug.h"
+#include "compat/c23.h"       // for nullptr, true, bool, false, size_t
 #include "host.h"
 #include "lib_common.h"
 #include "pixfmt_conv.h"
@@ -63,7 +63,7 @@ static const codec_t default_codecs[] = {I420, UYVY, YUYV, v210, R10k, R12L, RGB
 
 static const codec_t codecs_decklink[] = { R12L, R10k, v210, RGBA, UYVY };
 static const codec_t codecs_gl[] = { UYVY, v210, R10k, RGBA, RGB, RG48, Y416, DXT1, DXT1_YUV, DXT5 };
-const struct codec_profile_t {
+static const struct codec_profile_t {
         const char *name;
         const codec_t *codecs;
         int codec_count;
@@ -79,100 +79,104 @@ struct dummy_display_state {
         int rgb_shift[3];
 
         size_t dump_bytes;
-        _Bool dump_to_file;
+        bool dump_to_file;
         bool discard;       ///< do not recycle frame
-        _Bool oneshot;
-        _Bool raw;
+        bool oneshot;
+        bool raw;
         int dump_to_file_skip_frames;
 };
 
-static _Bool parse_codecs(char *str, codec_t *codecs, size_t *codec_count) {
+static bool parse_codecs(char *str, codec_t *codecs, size_t *codec_count) {
         for (unsigned i = 0; i < sizeof codec_profiles / sizeof codec_profiles[0]; ++i) {
                 if (strcmp(str, codec_profiles[i].name) == 0) {
                         memcpy(codecs, codec_profiles[i].codecs, sizeof(codec_t) * codec_profiles[i].codec_count);
                         *codec_count = codec_profiles[i].codec_count;
-                        return 1;
+                        return true;
                 }
         }
-        char *sptr = NULL;
-        char *tok = NULL;
+        char *sptr = nullptr;
+        char *tok = nullptr;
         *codec_count = 0;
         while ((tok = strtok_r(str, ",", &sptr))) {
-                str = NULL;
+                str = nullptr;
                 assert(*codec_count < sizeof ((struct dummy_display_state *) 0)->codecs / sizeof(codec_t));
                 codecs[(*codec_count)++] = get_codec_from_name(tok);
                 if (codecs[*codec_count - 1] == VIDEO_CODEC_NONE) {
                         log_msg(LOG_LEVEL_ERROR, MOD_NAME "Wrong codec spec: %s!\n", tok);
-                        return 0;
+                        return false;
                 }
         }
-        return 1;
+        return true;
 }
 
-static _Bool dummy_parse_opts(struct dummy_display_state *s, char *fmt) {
-        char *item = NULL;
-        char *save_ptr = NULL;
-        while ((item = strtok_r(fmt, ":", &save_ptr)) != NULL) {
-                fmt = NULL;
-                if (strstr(item, "codec=") != NULL) {
+static bool dummy_parse_opts(struct dummy_display_state *s, char *fmt) {
+        char *item = nullptr;
+        char *save_ptr = nullptr;
+        while ((item = strtok_r(fmt, ":", &save_ptr)) != nullptr) {
+                fmt = nullptr;
+                if (strstr(item, "codec=") != nullptr) {
                         if (!parse_codecs(strchr(item, '=') + 1, s->codecs, &s->codec_count)) {
-                                return 0;
+                                return false;
                         }
-                } else if (strstr(item, "dump") != NULL) {
-                        s->dump_to_file = 1;
-                } else if (strstr(item, "hexdump") != NULL) {
-                        if (strstr(item, "hexdump=") != NULL) {
+                } else if (strstr(item, "dump") != nullptr) {
+                        s->dump_to_file = true;
+                } else if (strstr(item, "hexdump") != nullptr) {
+                        if (strstr(item, "hexdump=") != nullptr) {
                                 s->dump_bytes = atoi(item + strlen("hexdump="));
                         } else {
                                 s->dump_bytes = DEFAULT_DUMP_LEN;
                         }
-                } else if (strstr(item, "rgb_shift=") != NULL) {
+                } else if (strstr(item, "rgb_shift=") != nullptr) {
                         item += strlen("rgb_shift=");
                         s->rgb_shift[0] = strtol(item, &item, 0);
                         item += 1;
                         s->rgb_shift[1] = strtol(item, &item, 0);
                         item += 1;
                         s->rgb_shift[2] = strtol(item, &item, 0);
-                } else if (strstr(item, "skip=") != NULL) {
+                } else if (strstr(item, "skip=") != nullptr) {
                         s->dump_to_file_skip_frames = atoi(strchr(item, '=') + 1);
                 } else if (strcmp(item, "oneshot") == 0) {
-                        s->oneshot = 1;
+                        s->oneshot = true;
                 } else if (strcmp(item, "raw") == 0) {
-                        s->raw = 1;
+                        s->raw = true;
                 } else if (strcmp(item, "discard") == 0) {
                         s->discard = true;
                 } else {
                         log_msg(LOG_LEVEL_ERROR, MOD_NAME "Unrecognized option: %s\n", item);
-                        return 0;
+                        return false;
                 }
         }
         if (!s->dump_to_file && (s->oneshot || s->dump_to_file_skip_frames > 0 || s->raw)) {
                 log_msg(LOG_LEVEL_WARNING, MOD_NAME "Some of passed options don't do anything without \":dump\".\n");
         }
-        return 1;
+        return true;
+}
+
+static void usage() {
+        char desc[] = "Display " TBOLD("dummy") " only consumes the video without displaying it. A difference to avoiding "
+                        "display specification is that it forces decoding (otherwise it will be skipped altogether).\n\n"
+                        "Additionally, options " TBOLD("hexdump") " and " TBOLD("dump") " are available for debugging.\n\n";
+        color_printf("%s", wrap_paragraph(desc));
+        struct key_val options[] = {
+                { "codec=<codec>[,<codec2>] | codec=<setlist>", "force the use of a codec instead of default set; special set list also possible (see below)" },
+                { "rgb_shift=<r>,<g>,<b>", "if using output codec RGBA, use specified shifts instead of default (" TOSTRING(DEFAULT_R_SHIFT) ", " TOSTRING(DEFAULT_G_SHIFT) ", " TOSTRING(DEFAULT_B_SHIFT) ")" },
+                { "dump[:skip=<n>][:oneshot][:raw]", "dump first frame to file dummy.<ext> (optionally skip <n> first frames); 'oneshot' - exit after dumping the picture; 'raw' - dump raw data" },
+                { "hexdump[=<n>]", "dump first n (default " TOSTRING(DEFAULT_DUMP_LEN) ") bytes of every frame in hexadecimal format" },
+                { "discard", "realloc every frame (do not recycle)" },
+                { nullptr, nullptr}
+        };
+        print_module_usage("-d dummy", options, nullptr, false);
+        color_printf("\nAvailable codec sets:\n");
+        for (unsigned i = 0; i < sizeof codec_profiles / sizeof codec_profiles[0]; ++i) {
+                color_printf("\t- " TBOLD("%s") "\n", codec_profiles[i].name);
+        }
 }
 
 static void *display_dummy_init(struct module *parent, const char *cfg, unsigned int flags)
 {
-        UNUSED(parent), UNUSED(flags);
+        UNUSED(parent); UNUSED(flags);
         if (strcmp(cfg, "help") == 0) {
-                char desc[] = "Display " TBOLD("dummy") " only consumes the video without displaying it. A difference to avoiding "
-                                "display specification is that it forces decoding (otherwise it will be skipped altogether).\n\n"
-                                "Additionally, options " TBOLD("hexdump") " and " TBOLD("dump") " are available for debugging.\n\n";
-                color_printf("%s", wrap_paragraph(desc));
-                struct key_val options[] = {
-                        { "codec=<codec>[,<codec2>] | codec=<setlist>", "force the use of a codec instead of default set; special set list also possible (see below)" },
-                        { "rgb_shift=<r>,<g>,<b>", "if using output codec RGBA, use specified shifts instead of default (" TOSTRING(DEFAULT_R_SHIFT) ", " TOSTRING(DEFAULT_G_SHIFT) ", " TOSTRING(DEFAULT_B_SHIFT) ")" },
-                        { "dump[:skip=<n>][:oneshot][:raw]", "dump first frame to file dummy.<ext> (optionally skip <n> first frames); 'oneshot' - exit after dumping the picture; 'raw' - dump raw data" },
-                        { "hexdump[=<n>]", "dump first n (default " TOSTRING(DEFAULT_DUMP_LEN) ") bytes of every frame in hexadecimal format" },
-                        { "discard", "realloc every frame (do not recycle)" },
-                        { NULL, NULL }
-                };
-                print_module_usage("-d dummy", options, NULL, 0);
-                color_printf("\nAvailable codec sets:\n");
-                for (unsigned i = 0; i < sizeof codec_profiles / sizeof codec_profiles[0]; ++i) {
-                        color_printf("\t- " TBOLD("%s") "\n", codec_profiles[i].name);
-                }
+                usage();
                 return INIT_NOERR;
         }
         struct dummy_display_state s = { .codec_count = sizeof default_codecs / sizeof default_codecs[0] };
@@ -183,7 +187,7 @@ static void *display_dummy_init(struct module *parent, const char *cfg, unsigned
         strcpy(ccpy, cfg);
 
         if (!dummy_parse_opts(&s, ccpy)) {
-                return NULL;
+                return nullptr;
         }
 
         struct dummy_display_state *ret = malloc(sizeof s);
@@ -223,7 +227,7 @@ static void dump_buf(unsigned char *buf, size_t len, int block_size) {
 static bool display_dummy_putf(void *state, struct video_frame *frame, long long flags)
 {
         struct dummy_display_state *s = state;
-        if (flags == PUTF_DISCARD || frame == NULL) {
+        if (flags == PUTF_DISCARD || frame == nullptr) {
                 if (s->discard) {
                         vf_free(frame);
                 }
@@ -292,21 +296,21 @@ static bool display_dummy_reconfigure(void *state, struct video_desc desc)
 
 static void display_dummy_probe(struct device_info **available_cards, int *count, void (**deleter)(void *)) {
         UNUSED(deleter);
-        *available_cards = NULL;
+        *available_cards = nullptr;
         *count = 0;
 }
 
 static const struct video_display_info display_dummy_info = {
         display_dummy_probe,
         display_dummy_init,
-        NULL, // _run
+        nullptr, // _run
         display_dummy_done,
         display_dummy_getf,
         display_dummy_putf,
         display_dummy_reconfigure,
         display_dummy_get_property,
-        NULL, // _put_audio_frame
-        NULL, // _reconfigure_audio
+        nullptr, // _put_audio_frame
+        nullptr, // _reconfigure_audio
         MOD_NAME,
 };
 

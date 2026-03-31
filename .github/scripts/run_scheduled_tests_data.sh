6f80d83c361e6fd2a09a892b0ccda89413318c55
Martin Pulec <martin.pulec@cesnet.cz>
2026-03-31 09:15:06 +0200

    CI semi-weekly tests: add some help tests
    after recent changes `-c help`/`-c lavc:help` crashes - create test for it

--- .github/scripts/run_scheduled_tests_data.sh
+++ .github/scripts/run_scheduled_tests_data.sh
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2026 CESNET, zájmové sdružení právnických osob
+#
 # test cases for run_scheduled_tests.sh
 #
 # For the documentation of add_test parameters see the
@@ -9,6 +12,16 @@ add_test --nonexistent-param                   should_fail
 add_test "-d sdl"                              should_timeout
 add_test "-t testcard -c lavc:e=libx265 -f rs -d dummy" should_timeout
 add_test "-t spout:check_lib"                  Windows_only
+
+# rxtx/compress help + incorrect options
+add_test "-c help -t testcard"
+add_test "-c lavc:help -t testcard"
+add_test "-x help"
+add_test "-x sdp:help"
+add_test "-c nonexistent"                      should_fail
+add_test "-c lavc:wrongopt -t testcard"        should_fail
+add_test "-x nonexistent"                      should_fail
+add_test "-x sdp:wrongopt"                     should_fail
 
 # reflector
 add_test -v                                    run_reflector # basic sanity test
